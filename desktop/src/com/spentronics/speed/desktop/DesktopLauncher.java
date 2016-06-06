860f3beef45bbf503519b70c785321fe00a5e367
Spencer <spencerflem@gmail.com>
2016-06-06 10:28:20 -0400

    Added autogen libgdx jazz

---
+++ desktop/src/com/spentronics/speed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spentronics.speed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spentronics.speed.SpeedGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpeedGame(), config);
+	}
+}
---
+++ html/src/com/spentronics/speed/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.spentronics.speed.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.spentronics.speed.SpeedGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SpeedGame();
+        }
+}
