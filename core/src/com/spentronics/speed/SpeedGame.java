6a79f2627d33a05ecff47d237e78e74472dba581
Spencer <spencerflem@gmail.com>
2016-06-06 11:13:41 -0400

    imported kryonet

--- core/src/com/spentronics/speed/SpeedGame.java
+++ core/src/com/spentronics/speed/SpeedGame.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.esotericsoftware.kryonet.Server;
 
 public class SpeedGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Server server;
 	
 	@Override
 	public void create () {
